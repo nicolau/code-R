34a720b05c9468279d98baf866e7a1857d9b313a
nicolau <anicolau85@gmail.com>
2018-08-12 23:26:28 -0300

    Update run_fgsea_function.R

--- run_fgsea_function.R
+++ run_fgsea_function.R
@@ -1,4 +1,4 @@
-run_fgsea <- function( pathwaysDatabase, ranksOfGenes, ranksFromR, minSizeGroup = 15, maxSizeGroup = 500, npermGroup = 1000, filterPathways = FALSE, pFilter = 0.05 ) {
+run_fgsea <- function( pathwaysDatabase, ranksOfGenes, minSizeGroup = 15, maxSizeGroup = 500, npermGroup = 1000, filterPathways = FALSE, pFilter = 0.05 ) {
   ##### Enrichment analysis
   ### Read GMT file
   gmtFile <- gmtPathways(pathwaysDatabase)
@@ -10,13 +10,8 @@ run_fgsea <- function( pathwaysDatabase, ranksOfGenes, ranksFromR, minSizeGroup 
       gmtFile[[i]] <- gmtFile[[i]][-exclude]
     }
   }
-  if(!is.empty(ranksOfGenes)) {
-    ranks <- read.table(ranksOfGenes, header = TRUE, colClasses = c("character", "numeric"))
-    ranks <- setNames(ranks[,2], ranks[,1])
-  }
-  else if(!is.empty(ranksFromR)) {
-    ranks <- ranksFromR
-  }
+  ranks <- read.table(ranksOfGenes, header = TRUE, colClasses = c("character", "numeric"))
+  ranks <- setNames(ranks[,2], ranks[,1])
   fgseaRes <- fgsea(pathways = gmtFile, stats = ranks, minSize = minSizeGroup, maxSize = maxSizeGroup, nperm = npermGroup)
   if(filterPathways) {
     fgseaRes <- fgseaRes[which(fgseaRes$padj <= pFilter),]
