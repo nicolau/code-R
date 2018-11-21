d5fddfe82d2c412518865c68c661c3dcaa6ff527
André Nicolau <anicolau85@gmail.com>
2018-11-20 23:57:21 -0200

    Update biomaRt_function.R

--- biomaRt_function.R
+++ biomaRt_function.R
@@ -15,15 +15,15 @@ getGeneSymbolFromTranscriptId <- function(values, type = c("transcriptId", "gene
   filterData <- NULL
   if(type == "transcriptId") {
     filterData <- "ensembl_transcript_id"
-    att <- c("ensembl_transcript_id", "transcript_start", "transcript_end")
+    att <- c("ensembl_transcript_id", "refseq_peptide")
   }
   else if(type == "geneId") {
     filterData <- "ensembl_gene_id"
-    att <- c("ensembl_gene_id", "transcript_start", "transcript_end")
+    att <- c("ensembl_gene_id", "refseq_peptide")
   }
   else if(type == "geneSymbol") {
     filterData <- symbol
-    att <- c("ensembl_transcript_id", "ensembl_gene_id", "transcript_start", "transcript_end") 
+    att <- c("ensembl_transcript_id", "ensembl_gene_id", "refseq_peptide") 
   }
   results <- getBM(attributes=c(symbol, "transcript_biotype", att), filters = filterData, values = values, mart = ensembl)
   if(onlyProteinCoding) {
