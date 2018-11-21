043dcc835c35fa793ac8c795935c9552ab3ad8a3
André Nicolau <anicolau85@gmail.com>
2018-11-21 00:05:07 -0200

    Update biomaRt_function.R

--- biomaRt_function.R
+++ biomaRt_function.R
@@ -15,7 +15,7 @@ getGeneSymbolFromTranscriptId <- function(values, type = c("transcriptId", "gene
   filterData <- NULL
   if(type == "transcriptId") {
     filterData <- "ensembl_transcript_id"
-    att <- c("ensembl_transcript_id", "transcript_count") 
+    att <- c("ensembl_transcript_id", "transcript_count")  
   }
   else if(type == "geneId") {
     filterData <- "ensembl_gene_id"
