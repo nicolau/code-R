fca160a991c30dc048f1755a8b63f3a9a7d68787
André Nicolau <anicolau85@gmail.com>
2018-11-24 17:21:13 -0200

    Update run_fgsea_function.R

--- run_fgsea_function.R
+++ run_fgsea_function.R
@@ -1,7 +1,7 @@
 run_fgsea <- function( pathwaysDatabase, ranksOfGenes, minSizeGroup = 15, maxSizeGroup = 500, npermGroup = 1000, filterPathways = FALSE, pFilter = 0.05 ) {
   ##### Enrichment analysis
-  ### Read GMT file
-  gmtFile <- gmtPathways(pathwaysDatabase)
+  ### Read GMT filegmtPathways()
+  gmtFile <- fgsea::gmtPathways(pathwaysDatabase)
   
   #exclude empty spaces in gmt
   for(i in 1:length(gmtFile)){
@@ -12,10 +12,9 @@ run_fgsea <- function( pathwaysDatabase, ranksOfGenes, minSizeGroup = 15, maxSiz
   }
   ranks <- read.table(ranksOfGenes, header = TRUE, colClasses = c("character", "numeric"))
   ranks <- setNames(ranks[,2], ranks[,1])
-  fgseaRes <- fgsea(pathways = gmtFile, stats = ranks, minSize = minSizeGroup, maxSize = maxSizeGroup, nperm = npermGroup)
+  fgseaRes <- fgsea::fgsea(pathways = gmtFile, stats = ranks, minSize = minSizeGroup, maxSize = maxSizeGroup, nperm = npermGroup)
   if(filterPathways) {
     fgseaRes <- fgseaRes[which(fgseaRes$padj <= pFilter),]
   }
   return(fgseaRes)
 }
-
