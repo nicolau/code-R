1b6f0df5479b8b0236b984db3dec397ce208f3f0
André Nicolau <anicolau85@gmail.com>
2018-12-10 23:06:42 -0200

    Update FoldChangeFiltering.R

--- FoldChangeFiltering.R
+++ FoldChangeFiltering.R
@@ -1,9 +1,9 @@
-FC_filter <- function() {
+FC_filter <- function(exp, samplesinfo, controlGroup, treatedGroup) {
   FCs <- data.frame(FC = rowMeans(exp[, which(as.character(samplesinfo$Class) == treatedGroup)]) /
     rowMeans(exp[, which(as.character(samplesinfo$Class) == controlGroup)]))
-
 
   is.na(FCs) <- sapply(FCs, is.infinite)
   is.na(FCs) <- sapply(FCs, is.nan)
   exp <- exp[which(!is.na(FCs)),]
+  exp
 }
