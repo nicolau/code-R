5ba7c415e7622997296da01e6e515bf942cf5ce9
André Nicolau <anicolau85@gmail.com>
2021-03-17 11:59:52 -0300

    Update MDP_execution.R

--- MDP_execution.R
+++ MDP_execution.R
@@ -1,7 +1,3 @@
-rm(list = ls())
-studies <- list()
-
-
 addStudy <- function(listStudies, ID, expression, phenodata, outputdir, control) {
   if(is.null(listStudies)) {
     listStudies <- list()
@@ -13,35 +9,38 @@ addStudy <- function(listStudies, ID, expression, phenodata, outputdir, control)
   return(listStudies)
 }
 
-studies <- addStudy(listStudies = studies, ID = "GSE0001", expression = "GSE0001_expression.tsv", phenodata = "GSE0001_expression.tsv", outputdir = "GSE0001", control = "control")
-studies <- addStudy(listStudies = studies, ID = "GSE0002", expression = "GSE0002_expression.tsv", phenodata = "GSE0002_expression.tsv", outputdir = "GSE0002", control = "uninfected")
-studies <- addStudy(listStudies = studies, ID = "GSE0003", expression = "GSE0003_expression.tsv", phenodata = "GSE0003_expression.tsv", outputdir = "GSE0003", control = "healthy")
-studies <- addStudy(listStudies = studies, ID = "GSE0004", expression = "GSE0004_expression.tsv", phenodata = "GSE0004_expression.tsv", outputdir = "GSE0004", control = "HC")
-studies <- addStudy(listStudies = studies, ID = "GSE0005", expression = "GSE0005_expression.tsv", phenodata = "GSE0005_expression.tsv", outputdir = "GSE0005", control = "control")
+runMDPListStudies <- function(studiesList) {
+  for(id in names(studiesList)) {
+    expressionFile <- studiesList[[id]][["expression"]]
+    phenodataFile  <- studiesList[[id]][["phenodata"]]
+    outputDir      <- studiesList[[id]][["outdir"]]
+    controlLabel   <- studiesList[[id]][["control"]]
 
+    exp       <- read.table(expressionFile, header = T, na.strings = "NA", sep = "\t")
+    phenodata <- read.table(phenodataFile,  header = T, na.strings = "NA", sep = "\t")
 
-for(id in names(studies)) {
-  expressionFile <- studies[[id]][["expression"]]
-  phenodataFile  <- studies[[id]][["phenodata"]]
-  outputDir      <- studies[[id]][["outdir"]]
-  controlLabel   <- studies[[id]][["control"]]
-  
-  exp       <- read.table(expressionFile, header = T, na.strings = "NA", sep = "\t")
-  phenodata <- read.table(phenodataFile,  header = T, na.strings = "NA", sep = "\t")
-  
-  phenodata <- phenodata[phenodata$Sample %in% colnames(exp), ]
-  exp       <- exp[,c("Symbol", as.character(phenodata$Sample))]
-  phenodata <- phenodata[phenodata$Sample %in% colnames(exp), ]
-  
-  rownames(exp) <- exp$Symbol
-  exp           <- exp[, 2:(ncol(exp))]
-  
-  MDP_dir <- outputDir
-  if(!dir.exists(MDP_dir)) {
-    dir.create(MDP_dir)
+    phenodata <- phenodata[phenodata$Sample %in% colnames(exp), ]
+    exp       <- exp[,c("Symbol", as.character(phenodata$Sample))]
+    phenodata <- phenodata[phenodata$Sample %in% colnames(exp), ]
+
+    rownames(exp) <- exp[, 1]
+    exp           <- exp[, 2:(ncol(exp))]
+
+    MDP_dir <- outputDir
+    if(!dir.exists(MDP_dir)) {
+      dir.create(MDP_dir)
+    }
+
+    mdp.results <- mdp(data = exp, pdata = phenodata, control_lab = controlLabel,
+                       file_name = paste(MDP_dir, "/MPD_", controlLabel, "_", sep=""))
   }
-  
-  mdp.results <- mdp(data = exp, pdata = phenodata, control_lab = controlLabel,
-                     file_name = paste(MDP_dir, "/MPD_", controlLabel, "_", sep=""))
-  
 }
+
+# Examples
+# studies <- list()
+# studies <- addStudy(listStudies = studies, ID = "GSE0001", expression = "GSE0001_expression.tsv", phenodata = "GSE0001_expression.tsv", outputdir = "GSE0001", control = "control")
+# studies <- addStudy(listStudies = studies, ID = "GSE0002", expression = "GSE0002_expression.tsv", phenodata = "GSE0002_expression.tsv", outputdir = "GSE0002", control = "uninfected")
+# studies <- addStudy(listStudies = studies, ID = "GSE0003", expression = "GSE0003_expression.tsv", phenodata = "GSE0003_expression.tsv", outputdir = "GSE0003", control = "healthy")
+# studies <- addStudy(listStudies = studies, ID = "GSE0004", expression = "GSE0004_expression.tsv", phenodata = "GSE0004_expression.tsv", outputdir = "GSE0004", control = "HC")
+# studies <- addStudy(listStudies = studies, ID = "GSE0005", expression = "GSE0005_expression.tsv", phenodata = "GSE0005_expression.tsv", outputdir = "GSE0005", control = "control")
+# runMDPListStudies(studies)
