7534919f5a975901bf22a0fb2e8bb720bca3ed87
André Nicolau <anicolau85@gmail.com>
2021-03-17 11:56:52 -0300

    Add files via upload

---
+++ MDP_execution.R
@@ -0,0 +1,47 @@
+rm(list = ls())
+studies <- list()
+
+
+addStudy <- function(listStudies, ID, expression, phenodata, outputdir, control) {
+  if(is.null(listStudies)) {
+    listStudies <- list()
+  }
+  listStudies[[ID]][["expression"]] <- expression
+  listStudies[[ID]][["phenodata"]]  <- phenodata
+  listStudies[[ID]][["control"]]    <- control
+  listStudies[[ID]][["outdir"]]     <- outputdir
+  return(listStudies)
+}
+
+studies <- addStudy(listStudies = studies, ID = "GSE0001", expression = "GSE0001_expression.tsv", phenodata = "GSE0001_expression.tsv", outputdir = "GSE0001", control = "control")
+studies <- addStudy(listStudies = studies, ID = "GSE0002", expression = "GSE0002_expression.tsv", phenodata = "GSE0002_expression.tsv", outputdir = "GSE0002", control = "uninfected")
+studies <- addStudy(listStudies = studies, ID = "GSE0003", expression = "GSE0003_expression.tsv", phenodata = "GSE0003_expression.tsv", outputdir = "GSE0003", control = "healthy")
+studies <- addStudy(listStudies = studies, ID = "GSE0004", expression = "GSE0004_expression.tsv", phenodata = "GSE0004_expression.tsv", outputdir = "GSE0004", control = "HC")
+studies <- addStudy(listStudies = studies, ID = "GSE0005", expression = "GSE0005_expression.tsv", phenodata = "GSE0005_expression.tsv", outputdir = "GSE0005", control = "control")
+
+
+for(id in names(studies)) {
+  expressionFile <- studies[[id]][["expression"]]
+  phenodataFile  <- studies[[id]][["phenodata"]]
+  outputDir      <- studies[[id]][["outdir"]]
+  controlLabel   <- studies[[id]][["control"]]
+  
+  exp       <- read.table(expressionFile, header = T, na.strings = "NA", sep = "\t")
+  phenodata <- read.table(phenodataFile,  header = T, na.strings = "NA", sep = "\t")
+  
+  phenodata <- phenodata[phenodata$Sample %in% colnames(exp), ]
+  exp       <- exp[,c("Symbol", as.character(phenodata$Sample))]
+  phenodata <- phenodata[phenodata$Sample %in% colnames(exp), ]
+  
+  rownames(exp) <- exp$Symbol
+  exp           <- exp[, 2:(ncol(exp))]
+  
+  MDP_dir <- outputDir
+  if(!dir.exists(MDP_dir)) {
+    dir.create(MDP_dir)
+  }
+  
+  mdp.results <- mdp(data = exp, pdata = phenodata, control_lab = controlLabel,
+                     file_name = paste(MDP_dir, "/MPD_", controlLabel, "_", sep=""))
+  
+}
