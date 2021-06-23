572f6e44c9f94ae2bc025041839be86535c21184
André Nicolau <anicolau85@gmail.com>
2021-06-23 20:32:39 -0300

    Update removeLowGenesExpression.R

--- removeLowGenesExpression.R
+++ removeLowGenesExpression.R
@@ -4,14 +4,14 @@ removeLowGenesExpression <- function(exp, minimumGeneExpression=0, percentageSam
 	exp$meanG     <- apply( exp[ , 2:ncol( exp ) ], 1, mean )
 	exp           <- exp[ order( exp[ , geneCol ], exp[ , 'meanG' ] ), ]
 	exp           <- exp[ !duplicated( exp[, geneCol] ), ]
-	rownames(exp) <- exp$Symbol
+	# rownames(exp) <- exp$Symbol
 
 	# Get all sample without mean column (last column)
 	# Run only variable minimumGeneExpression was defined
 	if(percentageSamples == 0) {
-		exp <- exp[ , 2:( ncol( exp ) - 1 ) ]
+		exp <- exp[ , c(geneCol, 2:( ncol( exp ) - 1 ) ) ]
 	} else if(percentageSamples > 0) {
-		exp2 <- exp[ , 2:( ncol( exp ) - 1 ) ]
+		exp2 <- exp[ , c(geneCol, 2:( ncol( exp ) - 1 )) ]
 		NumberOfSamplesMinimumValueAllowed <- round( (percentageSamples/100) * ncol( exp2 ) )
 		exp2 <- exp2[rowSums( exp2 <= minimumGeneExpression ) <= NumberOfSamplesMinimumValueAllowed, ]
 		exp <- exp2
