147fca406c5214b2c6c874cb7a85fcc55706a624
André Nicolau <anicolau85@gmail.com>
2022-02-24 20:06:55 +0000

    Update DEG_DESeq2.R
    Add parameter lfcShinkage

--- DEG_DESeq2.R
+++ DEG_DESeq2.R
@@ -1,9 +1,13 @@
-DEG_DESeq2 <- function(exp, samplesinfo, treatedGroup, controlGroup) {
+DEG_DESeq2 <- function(exp, samplesinfo, treatedGroup, controlGroup, lfcShrinkage = F) {
   contrasts <- c("Class", treatedGroup, controlGroup)
   dds <- DESeq2::DESeqDataSetFromMatrix(countData = exp, colData = samplesinfo, design = ~ Class)
   
   dds <- DESeq2::DESeq(dds)
   res <- DESeq2::results(dds, contrast = contrasts)
-  resApeT <- DESeq2::lfcShrink(dds, coef = 2, type = "apeglm", lfcThreshold = 1)
-  resApeT 
+  if(lfcShrinkage) {
+    resApeT <- DESeq2::lfcShrink(dds, coef = 2, type = "apeglm", lfcThreshold = 1)
+    return(resApeT)
+  } else {
+    return(res)
+  }
 }
